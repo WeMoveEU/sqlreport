0f07ee973fe949ce6fbc996c513717f1efdc1e1e
glilienthal <oliver@moldenhauer.org>
2016-03-05 19:29:40 +0100

    more time periods.

--- create_tables/create_tables.sql
+++ create_tables/create_tables.sql
@@ -30,8 +30,12 @@ insert into analytics_delta_t_h
 values 
 ("last six hours",6,0), 
 ("last 24 hours", 24,0), 
+("24 h before that", 48,24),
 ("last week (168h)", 7*24,0), 
-("last 30 days", 30*24,0); 
+("week before that", 2*7*24,7*24),
+("last 30 days", 30*24,0),
+("month before that ", 2*30*24,30*24)
+;
 
 
 drop table if exists analytics_member_metrics;
