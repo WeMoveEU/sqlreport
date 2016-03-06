1725c8afa478b93c75e5b8f0fed183991e84d975
glilienthal <oliver@moldenhauer.org>
2016-03-06 20:12:03 +0100

    members by country.

---
+++ queries/Members-by-country.sql
@@ -0,0 +1,21 @@
+SELECT 
+    *
+FROM
+    analytics_member_metrics
+WHERE
+    language = 'en_GB'; 
+
+
+
+SELECT 
+    country_id,
+    civicrm_country.iso_code,
+    civicrm_country.name,
+    SUM(number_added-number_removed) as members
+FROM
+    analytics_member_metrics
+        LEFT JOIN
+    civicrm_country ON civicrm_country.id = country_id
+GROUP BY country_id
+ORDER BY members DESC
+;
