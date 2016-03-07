6e65f67f95511043dbff4f3a3b48b409f23adf6c
glilienthal <oliver@moldenhauer.org>
2016-03-07 08:04:05 +0100

    fR:

---
+++ queries/Fr-members-by-country.sql
@@ -0,0 +1,13 @@
+SELECT 
+    civicrm_country.name,
+    SUM(number_added-number_removed) as members,
+    stamp as last_calculated
+FROM
+    analytics_member_metrics
+        LEFT JOIN
+    civicrm_country ON civicrm_country.id = country_id
+WHERE
+    language = 'fr_FR'
+GROUP BY country_id
+ORDER BY members DESC
+;
