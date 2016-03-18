f050022e84d626455f854b5a629ab3a586b03b8a
glilienthal <oliver@moldenhauer.org>
2016-03-18 08:55:59 +0100

    more join/leave. now dt.

--- temp/update-members-by-join-leave-dt.sql
+++ temp/update-members-by-join-leave-dt.sql
@@ -46,27 +46,36 @@ select * from analytics_member_metrics_dt;
 insert into analytics_member_metrics_dt
 (delta_t_h_id, number_added, number_removed, language)
 SELECT 
-    analytics_delta_t_h.id as delta_t_h_id,
+    delta_t_h_id,
     0,
     COUNT(*) AS count,
-    contact.preferred_language AS language
+    percontact.preferred_language AS language
 FROM
-    civicrm_contact contact
-        join analytics_delta_t_h 
-        JOIN
-    civicrm_email email ON email.contact_id = contact.id
-        AND email.is_primary IS TRUE
-        AND email.on_hold = 2
-       and email.hold_date <=  DATE_ADD(now(), INTERVAL - analytics_delta_t_h.hours_to hour)
-        and email.hold_date >=  DATE_ADD(now(), INTERVAL - analytics_delta_t_h.hours_from hour)
-        JOIN
-    civicrm_group_contact gc ON contact.id = gc.contact_id
-        AND gc.group_id = 42
-        AND gc.status = 'Added'
-        WHERE
-    contact.is_opt_out = 0
-        AND contact.is_deleted = 0
-GROUP BY delta_t_h_id, language; 
+    (SELECT 
+     analytics_delta_t_h.id as delta_t_h_id,
+ --       contact.id,
+        c.preferred_language as preferred_language
+    FROM
+    analytics_delta_t_h join
+    civicrm_activity a on  a.activity_type_id = @leave
+    and 
+    a.is_test=0
+    
+       and a.activity_date_time<=  DATE_ADD(now(), INTERVAL - analytics_delta_t_h.hours_to hour)
+        and a.activity_date_time>=  DATE_ADD(now(), INTERVAL - analytics_delta_t_h.hours_from hour)
+     
+   
+    join civicrm_activity_contact ac on a.id = ac.activity_id
+    join civicrm_contact c on c.id = ac.contact_id and source != "change.org" 
+/*    and
+    is_deleted = 0 AND is_opt_out = 0 
+    */
+--       and source != "change.org"
+    ) AS percontact
+GROUP BY delta_t_h_id, preferred_language ;
+
+
+
 
 
 SELECT * from analytics_member_metrics_dt;
