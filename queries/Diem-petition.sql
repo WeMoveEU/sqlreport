10fa5e16354610a51a99a1f0c6a463f4360c06e7
glilienthal <oliver@moldenhauer.org>
2016-03-25 14:46:27 +0100

    no variable allowed.

--- queries/Diem-petition.sql
+++ queries/Diem-petition.sql
@@ -1,5 +1,3 @@
-set @petition_signature:= 32;
-
 SELECT 
   asource.campaign_26 as local_group, count(contact.id) AS signatures
 FROM
@@ -9,7 +7,7 @@ FROM
         JOIN
     civicrm_activity activity ON ac.activity_id = activity.id
         AND activity.subject LIKE 'diem25%'
-        AND activity.activity_type_id = @petition_signature
+        AND activity.activity_type_id = 32
 	 join civicrm_value_action_source_4 asource on asource.entity_id=activity.id and asource.source_27='diem25volunteers'
 GROUP BY  local_group
 ORDER BY signatures DESC;
