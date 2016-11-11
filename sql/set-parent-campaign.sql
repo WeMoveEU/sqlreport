fef64b3ef50a22f168e379b287a4b29a1bd3b9a0
xavier <xavier@wemove.eu>
2016-11-11 12:25:13 +0100

    set the parent campaign to the EN one

---
+++ sql/set-parent-campaign.sql
@@ -0,0 +1,5 @@
+update civicrm_campaign c join (select id as p_id,name, SUBSTRING(name, 1, CHAR_LENGTH(name) - 3) AS parent from civicrm_campaign where name like "%_EN") p on c.name like concat(parent,"%") and c.parent_id is null set c.parent_id=p_id;
+
+update civicrm_campaign c join (select id as p_id,name, SUBSTRING(name, 1, CHAR_LENGTH(name) - 3) AS parent from civicrm_campaign where name like "%-EN") p on c.name like concat(parent,"%") and c.parent_id is null set c.parent_id=p_id;
+
+update civicrm_campaign c join (select id as p_id,name, SUBSTRING(name, 1, CHAR_LENGTH(name) - 6) AS parent from civicrm_campaign where name like "%INT-EN") p on c.name like concat(parent,"%") and c.parent_id is null set c.parent_id=p_id;
