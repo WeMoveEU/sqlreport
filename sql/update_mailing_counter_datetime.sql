4cf2bbd2ab1e0d8766bb79ff901d3102fe59f3dd
scardinius <scardinius@chords.pl>
2017-02-27 14:58:25 +0100

    Filter to current mailings on ab mailing report

--- sql/update_mailing_counter_datetime.sql
+++ sql/update_mailing_counter_datetime.sql
@@ -30,17 +30,17 @@ WHERE `value` IS NULL;
 INSERT IGNORE INTO analytics_temp_mailing
   SELECT m.id FROM civicrm_mailing m
     LEFT JOIN analytics_mailing_counter_datetime c ON c.mailing_id = m.id AND c.counter = 'median_original'
-  WHERE m.is_completed = 1 AND c.mailing_id IS NULL;
+  WHERE m.is_completed = 1 AND m.created_date >= '2017-01-01' AND c.mailing_id IS NULL;
 
 INSERT IGNORE INTO analytics_temp_mailing
   SELECT m.id FROM civicrm_mailing m
     LEFT JOIN analytics_mailing_counter_datetime c ON c.mailing_id = m.id AND c.counter = 'median_mailjet'
-  WHERE m.is_completed = 1 AND c.mailing_id IS NULL;
+  WHERE m.is_completed = 1 AND m.created_date >= '2017-01-01' AND c.mailing_id IS NULL;
 
 INSERT IGNORE INTO analytics_temp_mailing
   SELECT m.id FROM civicrm_mailing m
     LEFT JOIN analytics_mailing_counter_datetime c ON c.mailing_id = m.id AND c.counter = 'max_mailjet'
-  WHERE m.is_completed = 1 AND c.mailing_id IS NULL;
+  WHERE m.is_completed = 1 AND m.created_date >= '2017-01-01' AND c.mailing_id IS NULL;
 
 -- step 2. insert those which have newer events than date of preparing report
 INSERT IGNORE INTO analytics_temp_mailing
@@ -56,19 +56,21 @@ INSERT IGNORE INTO analytics_temp_mailing
 DELETE cd FROM analytics_mailing_counter_datetime cd JOIN analytics_temp_mailing tm ON tm.id = cd.mailing_id;
 
 
-
+-- step 4.1. insert median original
 INSERT IGNORE INTO analytics_mailing_counter_datetime
   SELECT m.id, 'median_original', analyticsMedianOriginalTimeStamp(m.id), NOW()
   FROM civicrm_mailing m
     JOIN analytics_temp_mailing tm ON tm.id = m.id
   WHERE m.is_completed = 1;
 
+-- step 4.2. insert median mailjet
 INSERT IGNORE INTO analytics_mailing_counter_datetime
   SELECT m.id, 'median_mailjet', analyticsMedianTimeStamp(m.id), NOW()
   FROM civicrm_mailing m
     JOIN analytics_temp_mailing tm ON tm.id = m.id
   WHERE m.is_completed = 1;
 
+-- step 4.3. insert max of mailjet event
 -- time_stamp include date from mailjet
 -- original_time_stamp > '1970-01-01' means that we get records with all data
 INSERT IGNORE INTO analytics_mailing_counter_datetime
