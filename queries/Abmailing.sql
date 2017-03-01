547873a4f97061a040a9369bbdc9ababb2347ec4
scardinius <scardinius@chords.pl>
2017-03-01 10:03:00 +0100

    (xavier) Improve query for ab mailing report

--- queries/Abmailing.sql
+++ queries/Abmailing.sql
@@ -6,7 +6,7 @@ SELECT
   WHERE status = 'Complete' AND is_test = 0 AND mailing_id = m.id) start_date,
   (SELECT max(end_date)
   FROM civicrm_mailing_job
-  WHERE status = 'Complete' AND is_test = 0 AND mailing_id = m.id) sent_date,
+  WHERE status = 'Complete' AND is_test = 0 AND mailing_id = m.id) end_date,
   co.value median_original,
   cm.value median_mailjet,
   cm2.value max_mailjet
@@ -15,4 +15,4 @@ FROM civicrm_mailing m
   LEFT JOIN analytics_mailing_counter_datetime cm ON cm.mailing_id = m.id AND cm.counter = 'median_mailjet'
   LEFT JOIN analytics_mailing_counter_datetime cm2 ON cm2.mailing_id = m.id AND cm2.counter = 'max_mailjet'
 WHERE m.is_completed = 1
-ORDER BY id DESC;
+ORDER BY start_date DESC;
