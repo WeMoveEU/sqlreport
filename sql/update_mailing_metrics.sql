982c2a4ac5d546a428fd1e5f9d816eef52ee5ab9
xavier <github@tttp.eu>
2017-05-02 10:16:08 +0200

    set a shorter time window to look at after a mailing (10 instead of 100 days)

--- sql/update_mailing_metrics.sql
+++ sql/update_mailing_metrics.sql
@@ -1,3 +1,4 @@
+SELECT @timeslot := 10; -- how many days to analyse after the mailing
 
 -- Recipients
 -- INSERT IGNORE does not work because A/B mailing recipients may not be correct until they are sent
@@ -22,7 +23,7 @@ INSERT INTO data_mailing_counter
     JOIN civicrm_mailing_event_queue q ON q.job_id=j.id
     JOIN civicrm_mailing_event_opened o ON o.event_queue_id=q.id
     JOIN data_timeboxes b ON TIMESTAMPDIFF(MINUTE, j.start_date, o.time_stamp)<b.box
-    WHERE j.is_test=0 AND TIMESTAMPADD(DAY, 100, j.start_date) > NOW()
+    WHERE j.is_test=0 AND TIMESTAMPADD(DAY, @timeslot , j.start_date) > NOW()
     GROUP BY j.mailing_id, b.box
   ON DUPLICATE KEY UPDATE value=VALUES(value), last_updated=NOW();
 
@@ -33,7 +34,7 @@ INSERT INTO data_mailing_counter
     JOIN civicrm_mailing_event_queue q ON q.job_id=j.id
     JOIN civicrm_mailing_event_trackable_url_open o ON o.event_queue_id=q.id
     JOIN data_timeboxes b ON TIMESTAMPDIFF(MINUTE, j.start_date, o.time_stamp)<b.box
-    WHERE j.is_test=0 AND TIMESTAMPADD(DAY, 100, j.start_date) > NOW()
+    WHERE j.is_test=0 AND TIMESTAMPADD(DAY, @timeslot, j.start_date) > NOW()
     GROUP BY j.mailing_id, b.box
   ON DUPLICATE KEY UPDATE value=VALUES(value), last_updated=NOW();
 
@@ -44,7 +45,7 @@ INSERT INTO data_mailing_counter
     JOIN civicrm_mailing_event_queue q ON q.job_id=j.id
     JOIN civicrm_mailing_event_unsubscribe o ON o.event_queue_id=q.id
     JOIN data_timeboxes b ON TIMESTAMPDIFF(MINUTE, j.start_date, o.time_stamp)<b.box
-    WHERE j.is_test=0 AND TIMESTAMPADD(DAY, 100, j.start_date) > NOW()
+    WHERE j.is_test=0 AND TIMESTAMPADD(DAY, @timeslot, j.start_date) > NOW()
     GROUP BY j.mailing_id, b.box
   ON DUPLICATE KEY UPDATE value=VALUES(value), last_updated=NOW();
 
@@ -52,7 +53,7 @@ INSERT INTO data_mailing_counter
 -- Assumes that the status of shares is always the same (Completed)
 -- Force order of joins for MySQL 5.5, could be removed in later versions
 INSERT INTO data_mailing_counter
-  SELECT STRAIGHT_JOIN SUBSTRING(s.source_27, 10), 
+  SELECT  SUBSTRING(s.source_27, 10), 
       IF(a.activity_type_id=32,
         CASE
           WHEN a.status_id=9 THEN 'new_direct_signs'
@@ -73,7 +74,7 @@ INSERT INTO data_mailing_counter
     WHERE (a.activity_type_id=32 OR a.activity_type_id=54)
       AND a.status_id IN (1, 2, 4, 9)
       AND s.source_27 LIKE 'civimail-%'
-      AND TIMESTAMPADD(DAY, 100, j.start_date) > NOW()
+      AND TIMESTAMPADD(DAY, @timeslot, j.start_date) > NOW()
     GROUP BY s.source_27, a.activity_type_id, a.status_id, b.box
   ON DUPLICATE KEY UPDATE value=VALUES(value), last_updated=NOW();
 
@@ -101,7 +102,7 @@ INSERT INTO data_mailing_counter
     JOIN (SELECT mailing_id, MIN(start_date) AS start_date FROM civicrm_mailing_job WHERE is_test=0 GROUP BY mailing_id) j 
       ON j.mailing_id=SUBSTRING(source.source_27, 10)
     JOIN data_timeboxes b ON TIMESTAMPDIFF(MINUTE, j.start_date, inf_a.activity_date_time)<b.box
-    WHERE TIMESTAMPADD(DAY, 100, j.start_date) > NOW()
+    WHERE TIMESTAMPADD(DAY, @timeslot, j.start_date) > NOW()
     GROUP BY source.source_27, inf_a.activity_type_id, inf_a.status_id, b.box
   ON DUPLICATE KEY UPDATE value=VALUES(value), last_updated=NOW();
 
