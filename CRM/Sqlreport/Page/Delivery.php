0fd2402ba780fe604e2413f90d02ca5657496c92
xavier <github@tttp.eu>
2017-05-05 18:02:38 +0200

    export to csv, two names per line

--- CRM/Sqlreport/Page/Delivery.php
+++ CRM/Sqlreport/Page/Delivery.php
@@ -14,44 +14,29 @@ class CRM_Sqlreport_Page_Delivery extends CRM_Core_Page {
     }
 
     $request = explode('/',$request);
-    $idt = NULL;
-print_r($request);
-die ("toto");
+    $id = 0;
     if (CRM_Utils_Array::value(2, $request)) {
-      $tplfile = _civicrm_api_get_camel_name($request[2]);
-      $tplfile = explode('?', $tplfile);
-      $tpl = 'dataviz/'.$tplfile[0].'.tpl';
+      $id += $request[2];
     }
-    $dummy = NULL;
-    if (array_key_exists('id',$_GET)) {// special treatmenent, because it's often used
-      $smarty->assign ('id',(int)$_GET['id']);// an id is always positive
-    }
-    $pos = strpos (implode (array_keys ($_GET)),'<') ;
-
-    if ($pos !== false) {
-      die ("SECURITY FATAL: one of the param names contains &lt;");
-    }
-    $param = array_map( 'htmlentities' , $_GET);
-//TODO: sql escape the params too
-    unset($param['q']);
-    $smarty->assign_by_ref("request", $param);
-
-   CRM_Core_Resources::singleton()
-    ->addScriptFile('eu.tttp.civisualize', 'js/d3.v3.js', 110, 'html-header', FALSE)
-    ->addScriptFile('eu.tttp.civisualize', 'js/dc/dc.js', 110, 'html-header', FALSE)
-    ->addScriptFile('eu.tttp.civisualize', 'js/dc/crossfilter.js', 110, 'html-header', FALSE)
-    ->addScriptFile('eu.tttp.civisualize', 'js/filesaver.js', 110, 'html-header', FALSE)
-    ->addScriptFile('eu.tttp.civisualize', 'js/common.js', 110, 'html-header', FALSE)
-    ->addStyleFile('eu.tttp.civisualize', 'js/dc/dc.css')
-    ->addStyleFile('eu.tttp.civisualize', 'css/style.css')
-    ->addVars('civisualize', array(
-       'baseUrl' => CRM_Core_Resources::singleton()->getUrl('eu.tttp.civisualize'),
-   ));
-
     require_once 'CRM/Core/Smarty/plugins/function.crmSQL.php';
     $smarty->register_function("crmSQL", "smarty_function_crmSQL");
 
-    parent::run();
+    $r = smarty_function_crmSQL(array("json"=>"Delivery", "id"=>$id));
+
+    $filename = "delivery_".$id;
+
+//    header("Content-type: text/csv");
+//    header("Content-Disposition: attachment; filename={$filename}.csv");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    $csv= fopen("php://output", 'w');
+    fputs($csv, chr(0xEF) . chr(0xBB) . chr(0xBF) ); //excel bom fix utf
+    $max=count($r["values"]);
+    for ($i = 0; $i <= $max; $i+=2) {
+      fputcsv($csv,array_merge(array_values($r["values"][$i]),array_values($r["values"][$i+1])));
+    }
+    fclose($csv);
+die ("");
   }
 
 }
