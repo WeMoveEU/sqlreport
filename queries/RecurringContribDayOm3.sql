d3aea2b61a3d985308ff06938e07f03644b33c11
Romain Thouvenin <romain@wemove.eu>
2017-05-22 15:55:15 +0200

    Filter out test contributions

--- queries/RecurringContribDayOm3.sql
+++ queries/RecurringContribDayOm3.sql
@@ -1,16 +1,14 @@
 SELECT 
-
     utm_campaign_33 AS camp,
-em.email as email,
-create_date AS date_time,
+    em.email as email,
+    create_date AS date_time,
     R.currency,
     pp.name,
     status.name AS status_contrib,
     M.status,
     frequency_unit AS frequency,
     COUNT(*) AS nb,
-amount
-
+    amount
 FROM
     civicrm_contribution_recur AS R
         JOIN
@@ -26,7 +24,6 @@ FROM
         LEFT JOIN
     civicrm_value_utm_5 utm ON c.id = utm.entity_id
    left join civicrm_email em on c.contact_id=em.contact_id and em.is_primary=1
+WHERE R.is_test=0 AND c.is_test=0
 GROUP BY utm_campaign_33 ,  email, R.contribution_status_id , pp.id , status , currency , frequency_unit, amount
 ORDER BY camp DESC; 
-
-#select date(create_date) as date, currency, pp.name, status.name as status_contrib, M.status, frequency_unit as frequency, count(*) as nb, sum(amount) sum, avg(amount) avg from civicrm_contribution_recur  as R join civicrm_payment_processor pp on payment_processor_id=pp.id left join civicrm_option_value status on option_group_id=11 and contribution_status_id=status.value left join civicrm_sdd_mandate M on R.id=M.entity_id and M.entity_table="civicrm_contribution_recur" join  civicrm_contribution c on contribution_recur_id=R.id left join civicrm_value_utm_5 utm on c.id=entity_id limit 10 group by utm_campaign_30, date(create_date), contribution_status_id,pp.id,status, currency, frequency_unit order by date desc;
