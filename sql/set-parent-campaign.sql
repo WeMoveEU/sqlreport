5132b8f0a1ca096b9146bf48606ffe7a75c8eb6c
Xavier Dutoit <github@tttp.eu>
2017-06-20 14:47:25 +0200

    put -UK-EN under -INT-EN

--- sql/set-parent-campaign.sql
+++ sql/set-parent-campaign.sql
@@ -3,3 +3,6 @@ update civicrm_campaign c join (select id as p_id,name, SUBSTRING(name, 1, CHAR_
 update civicrm_campaign c join (select id as p_id,name, SUBSTRING(name, 1, CHAR_LENGTH(name) - 3) AS parent from civicrm_campaign where name like "%-EN") p on c.name like concat(parent,"%") and c.parent_id is null set c.parent_id=p_id;
 
 update civicrm_campaign c join (select id as p_id,name, SUBSTRING(name, 1, CHAR_LENGTH(name) - 6) AS parent from civicrm_campaign where name like "%INT-EN") p on c.name like concat(parent,"%") and c.parent_id is null set c.parent_id=p_id;
+
+#update civicrm_campaign c join (select id as p_id,name, SUBSTRING(name, 1, CHAR_LENGTH(name) - 6) AS parent, name as p_name from civicrm_campaign where name like "%INT-EN") p on c.name like concat(parent,"%")  and (c.parent_id is null or c.parent_id=c.id)  set c.parent_id=p_id where c.name like "%UK-EN";
+
