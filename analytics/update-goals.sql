6982ba2b13edfc78232347a274694290087e2eb6
scardinius <scardinius@chords.pl>
2017-08-01 14:44:34 +0200

    Update actual values for recurring donation goals

--- analytics/update-goals.sql
+++ analytics/update-goals.sql
@@ -20,4 +20,30 @@ UPDATE
       GROUP BY scope, `begin`, `end`
   ) val ON val.scope=goal.scope AND val.begin=goal.begin AND val.end=goal.end
   SET goal.actual=val.growth
-  WHERE goal.metric='active_member_growth'
+  WHERE goal.metric='active_member_growth';
+
+
+-- Recurring donations per languages
+UPDATE analytics_goals_dates g2
+  JOIN (SELECT
+    g.scope, g.begin, g.end, sum(cr.total_amount) growth
+  FROM civicrm_contribution cr
+    JOIN analytics_goals_dates g
+      ON g.metric = 'recurring_donations' AND (cr.receive_date >= g.begin AND cr.receive_date <= g.end)
+    JOIN civicrm_contact c ON c.id = cr.contact_id AND c.preferred_language COLLATE utf8_unicode_ci = g.scope
+  WHERE cr.contribution_recur_id IS NOT NULL AND contribution_status_id = 1 AND is_test = 0
+  GROUP BY g.scope, g.begin, g.end) t ON g2.scope = t.scope AND g2.begin = t.begin AND g2.end = t.end
+SET g2.actual = t.growth
+WHERE g2.metric = 'recurring_donations';
+
+-- Recurring donations per organization
+UPDATE analytics_goals_dates g2
+  JOIN (SELECT
+    g.begin, g.end, sum(cr.total_amount) growth
+  FROM civicrm_contribution cr
+    JOIN analytics_goals_dates g
+      ON g.metric = 'recurring_donations' AND (cr.receive_date >= g.begin AND cr.receive_date <= g.end)
+  WHERE g.scope = 'organization' AND cr.contribution_recur_id IS NOT NULL AND contribution_status_id = 1 AND is_test = 0
+  GROUP BY g.begin, g.end) t ON g2.begin = t.begin AND g2.end = t.end
+SET g2.actual = t.growth
+WHERE g2.metric = 'recurring_donations' AND g2.scope = 'organization';
