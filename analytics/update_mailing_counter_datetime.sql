bd9cbfa6ec88610ad03561be997a260752006265
scardinius <scardinius@chords.pl>
2017-09-08 12:31:21 +0200

    Fix update mailing counter datetime - set columns for insert

--- analytics/update_mailing_counter_datetime.sql
+++ analytics/update_mailing_counter_datetime.sql
@@ -1,17 +1,17 @@
 
 
 -- update renormalized ab mailings tables
-INSERT IGNORE INTO data_mailing_ab
+INSERT IGNORE INTO data_mailing_ab (id, mailing_type, mailing_abtest_id, mailing_abtest_type)
   SELECT
     ab.mailing_id_a, m.mailing_type, ab.id, 'a'
   FROM civicrm_mailing_abtest ab JOIN civicrm_mailing m ON m.id = ab.mailing_id_a;
 
-INSERT IGNORE INTO data_mailing_ab
+INSERT IGNORE INTO data_mailing_ab (id, mailing_type, mailing_abtest_id, mailing_abtest_type)
   SELECT
     ab.mailing_id_b, m.mailing_type, ab.id, 'b'
   FROM civicrm_mailing_abtest ab JOIN civicrm_mailing m ON m.id = ab.mailing_id_b;
 
-INSERT IGNORE INTO data_mailing_ab
+INSERT IGNORE INTO data_mailing_ab (id, mailing_type, mailing_abtest_id, mailing_abtest_type)
   SELECT
     ab.mailing_id_c, m.mailing_type, ab.id, 'c'
   FROM civicrm_mailing_abtest ab JOIN civicrm_mailing m ON m.id = ab.mailing_id_c;
