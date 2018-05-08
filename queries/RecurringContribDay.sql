a43fe8bc31b02c5463a0760129bf40b16bf38ec3
scardinius <scardinius@chords.pl>
2018-05-08 10:54:34 +0200

    Fix RecurringContriDay query, nonaggregated column

--- queries/RecurringContribDay.sql
+++ queries/RecurringContribDay.sql
@@ -1,5 +1,5 @@
 select 
-  utm_campaign_33 as camp, date(create_date) as date, R.currency, pp.name, status.name as status_contrib, M.status, 
+  utm_campaign_33 as camp, date(create_date) as date, R.currency, pp.name, max(status.name) as status_contrib, M.status,
   frequency_unit as frequency, count(*) as nb, sum(amount) sum, avg(amount) avg 
 from civicrm_contribution_recur  as R 
 left join civicrm_payment_processor pp on payment_processor_id=pp.id
@@ -8,5 +8,5 @@ left join civicrm_sdd_mandate M on R.id=M.entity_id and M.entity_table="civicrm_
 join  civicrm_contribution c on contribution_recur_id=R.id 
 left join civicrm_value_utm_5 utm on c.id=utm.entity_id 
 where R.is_test=0 and c.is_test=0
-group by utm_campaign_33, date(create_date), R.contribution_status_id,pp.id,status, currency, frequency_unit 
+group by utm_campaign_33, date(create_date), R.contribution_status_id, pp.id, status, currency, frequency_unit
 order by date desc; 
