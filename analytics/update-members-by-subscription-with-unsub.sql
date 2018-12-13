ba672c0b78b1b85083f4bf2135e2df23799de46e
Romain Thouvenin <romain@wemove.eu>
2018-12-13 06:00:02 +0100

    Ignore contact and email status when computing membership history

--- analytics/update-members-by-subscription-with-unsub.sql
+++ analytics/update-members-by-subscription-with-unsub.sql
@@ -1,78 +1,42 @@
-insert into analytics_calculation_times
-( calculation ) 
-values ("before member calculation 1");
+INSERT INTO analytics_calculation_times ( calculation ) VALUES ("before member calculation 1");
 
+TRUNCATE TABLE analytics_member_metrics;
 
-
-truncate table analytics_member_metrics; 
-
-insert into analytics_member_metrics
-(number_added, number_removed, added_date, language, country_id)
-SELECT 
-    COUNT(*) AS count, 0, percontact.added_date AS added_date, percontact.preferred_language as language, percontact.country_id as country_id
-FROM	
-    (SELECT 
-        contact.id,
-            MAX(DATE(hist.date)) AS added_date,
-            contact.preferred_language as preferred_language,
-            MIN(address.country_id) as country_id
-    FROM
-        civicrm_contact AS contact 
-/*    JOIN civicrm_group_contact gc ON contact.id = gc.contact_id
-        AND gc.group_id = 42
-        AND gc.status in ('Added','Removed')
-        */
-    JOIN civicrm_subscription_history hist ON hist.contact_id = contact.id
-        AND hist.group_id = 42
-        AND hist.status = 'Added'
-	join civicrm_email em on em.contact_id=contact.id
-        and em.is_primary is true
-        and on_hold in(0,2) 
-	left JOIN
-    civicrm_address address ON address.contact_id = contact.id
-    and address.is_primary=1        
-	WHERE
-        is_deleted = 0 AND is_opt_out = 0   
-	GROUP BY contact.id) AS percontact
-GROUP BY added_date, country_id, preferred_language;
-
-
-insert into analytics_calculation_times
-( calculation ) 
-values ("after member calculation 1");
-
-
-
-
-insert into analytics_member_metrics
-(number_added, number_removed, added_date, language, country_id)
-SELECT 
+INSERT INTO analytics_member_metrics
+  (number_added, number_removed, added_date, language, country_id)
+  SELECT
+    COUNT(*),
     0,
-    COUNT(*) AS number_removed,
-    DATE(email.hold_date) AS added_date,
-    contact.preferred_language AS language,
+    DATE(hist.date) AS added_date,
+    preferred_language,
     country_id
-FROM
-    civicrm_contact contact
-        JOIN
-    civicrm_email email ON email.contact_id = contact.id
-        AND email.is_primary IS TRUE
-        AND email.on_hold = 2
-        JOIN
-    civicrm_group_contact gc ON contact.id = gc.contact_id
-        AND gc.group_id = 42
-        AND gc.status = 'Removed'
-	left JOIN
-    civicrm_address address ON address.contact_id = contact.id
-    and address.is_primary=1        
-WHERE
-    contact.is_opt_out = 0
-        AND contact.is_deleted = 0
-GROUP BY added_date, language, country_id; 
 
+  FROM civicrm_contact AS contact
+  JOIN civicrm_subscription_history hist
+    ON hist.contact_id = contact.id AND hist.group_id = 42 AND hist.status = 'Added'
+  LEFT JOIN civicrm_address address
+    ON address.contact_id = contact.id AND address.is_primary
 
+  GROUP BY added_date, preferred_language, country_id
+;
 
-insert into analytics_calculation_times
-( calculation ) 
-values ("after member calculation 2");
+INSERT INTO analytics_calculation_times ( calculation ) VALUES ("after member calculation 1");
+
+INSERT INTO analytics_member_metrics
+(number_added, number_removed, added_date, language, country_id)
+  SELECT
+    0,
+    COUNT(*),
+    DATE(hist.date) AS added_date,
+    preferred_language,
+    country_id
+
+  FROM civicrm_contact contact
+  JOIN civicrm_subscription_history hist
+    ON contact.id = hist.contact_id AND hist.group_id = 42 AND hist.status = 'Removed'
+	LEFT JOIN civicrm_address address
+    ON address.contact_id = contact.id AND address.is_primary
+  GROUP BY added_date, preferred_language, country_id;
+
+INSERT INTO analytics_calculation_times ( calculation ) VALUES ("after member calculation 2");
 
