979323faea6ca034e7ff7b479ffef8f0876b86c5
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-04-07 15:17:27 +0200

    Only scan recurring donors when updating the recurring donor segment

--- sql/update-contact-segments.sql
+++ sql/update-contact-segments.sql
@@ -2,29 +2,24 @@
 
 -- Create the value of recurring_donor segment
 INSERT INTO civicrm_value_contact_segments (entity_id, recurring_donor)
-  SELECT
-    c.id,
-    IF(contact_id IS NULL, 0, segment_value)
-  FROM civicrm_contact c
-  LEFT JOIN (
-    SELECT
-        r.contact_id AS contact_id,
-        MAX(
-          CASE
-            WHEN d.id IS NULL AND r.cancel_date IS NOT NULL THEN 1 -- failed
-            WHEN d.id IS NULL THEN 3 -- current (new)
-            WHEN d.contribution_status_id != 1 THEN 1 -- failed
-            WHEN r.cancel_date IS NOT NULL OR r.end_date IS NOT NULL THEN 2 -- past
-            ELSE 3 -- current
-          END
-        ) AS segment_value
-      FROM civicrm_contribution_recur r
-      LEFT JOIN civicrm_contribution d ON r.id=d.contribution_recur_id
-      WHERE r.is_test = 0
-      GROUP BY contact_id
-  ) v ON v.contact_id=c.id
-  ON DUPLICATE KEY UPDATE recurring_donor=VALUES(recurring_donor)
-;
+SELECT recur.contact_id,
+    MAX(
+        CASE
+            WHEN donation.id IS NULL
+            AND recur.cancel_date IS NOT NULL THEN 1
+            WHEN donation.id IS NULL THEN 3
+            WHEN donation.contribution_status_id != 1 THEN 1
+            WHEN recur.cancel_date IS NOT NULL
+            OR recur.end_date IS NOT NULL THEN 2
+            ELSE 3
+        END
+    ) recurring_donor
+FROM civicrm_contribution_recur recur
+    LEFT JOIN civicrm_contribution donation ON recur.id = donation.contribution_recur_id
+WHERE recur.is_test = 0
+GROUP BY contact_id ON DUPLICATE KEY
+UPDATE recurring_donor =
+VALUES(recurring_donor);
 
 BEGIN;
 -- Active members segment: pre-fill everyone as not member or inactive
