a1e16154037c47a00089de4109b523d4470dbb07
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-07-29 18:10:19 +0200

    More conversions to SELECT INTO / LOAD DATA

--- sql/nightly/update-iced-contacts.sql
+++ sql/nightly/update-iced-contacts.sql
@@ -5,37 +5,50 @@ CREATE TABLE tmp_6months_actions (
   contact_id INT UNSIGNED PRIMARY KEY
 );
 -- opens
-INSERT IGNORE INTO tmp_6months_actions
-  SELECT q.contact_id
-  FROM civicrm_mailing_event_opened o1
-    JOIN civicrm_mailing_event_queue q ON o1.event_queue_id = q.id
-  WHERE o1.time_stamp BETWEEN DATE_ADD(DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH), INTERVAL -3 DAY)
-  AND DATE_ADD(CURRENT_DATE, INTERVAL -3 DAY);
+
+SELECT q.contact_id
+FROM civicrm_mailing_event_opened o1
+  JOIN civicrm_mailing_event_queue q ON o1.event_queue_id = q.id
+WHERE o1.time_stamp BETWEEN DATE_ADD(DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH), INTERVAL -3 DAY)
+AND DATE_ADD(CURRENT_DATE, INTERVAL -3 DAY)
+INTO OUTFILE '/var/lib/mysql-files/nightly-iced-opens-%NOW%.tsv';
+
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-iced-opens-%NOW%.tsv' 
+IGNORE INTO TABLE tmp_6months_actions
+(contact_id);
 
 -- clicks
-INSERT IGNORE INTO tmp_6months_actions
-  SELECT q.contact_id
-  FROM civicrm_mailing_event_trackable_url_open o1
-    JOIN civicrm_mailing_event_queue q ON o1.event_queue_id = q.id
-  WHERE o1.time_stamp BETWEEN DATE_ADD(DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH), INTERVAL -3 DAY)
-  AND DATE_ADD(CURRENT_DATE, INTERVAL -3 DAY);
+SELECT q.contact_id
+FROM civicrm_mailing_event_trackable_url_open o1
+  JOIN civicrm_mailing_event_queue q ON o1.event_queue_id = q.id
+WHERE o1.time_stamp BETWEEN DATE_ADD(DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH), INTERVAL -3 DAY)
+AND DATE_ADD(CURRENT_DATE, INTERVAL -3 DAY)
+INTO OUTFILE '/var/lib/mysql-files/nightly-iced-clicks-%NOW%.tsv';
+
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-iced-clicks-%NOW%.tsv' 
+IGNORE INTO TABLE tmp_6months_actions
+(contact_id);
 
 -- list of all iced contacts
 DROP TABLE IF EXISTS tmp_6months;
 CREATE TABLE tmp_6months (
   id INT UNSIGNED PRIMARY KEY
 );
-INSERT IGNORE INTO tmp_6months
-  SELECT
-    eq.contact_id
-  FROM civicrm_mailing_event_delivered ed
-    JOIN civicrm_mailing_event_queue eq ON eq.id = ed.event_queue_id
-    JOIN civicrm_contact c ON c.id = eq.contact_id
-    LEFT JOIN tmp_6months_actions a ON a.contact_id = eq.contact_id
-  WHERE ed.time_stamp BETWEEN DATE_ADD(DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH), INTERVAL -3 DAY)
-  AND DATE_ADD(CURRENT_DATE, INTERVAL -3 DAY)
-  AND c.created_date < DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH)
-      AND a.contact_id IS NULL;
+
+SELECT
+  eq.contact_id
+FROM civicrm_mailing_event_delivered ed
+  JOIN civicrm_mailing_event_queue eq ON eq.id = ed.event_queue_id
+  JOIN civicrm_contact c ON c.id = eq.contact_id
+  LEFT JOIN tmp_6months_actions a ON a.contact_id = eq.contact_id
+WHERE ed.time_stamp BETWEEN DATE_ADD(DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH), INTERVAL -3 DAY)
+AND DATE_ADD(CURRENT_DATE, INTERVAL -3 DAY)
+AND c.created_date < DATE_ADD(CURRENT_DATE, INTERVAL -6 MONTH)
+    AND a.contact_id IS NULL
+INTO OUTFILE '/var/lib/mysql-files/nightly-iced-contacts-%NOW%.tsv';
+
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-iced-contacts-%NOW%.tsv'
+IGNORE INTO TABLE tmp_6months (id);
 
 -- add if needed
 SET @iced_count = (SELECT sum(result)
@@ -48,11 +61,15 @@ DROP TABLE IF EXISTS tmp_6months_remove;
 CREATE TABLE tmp_6months_remove (
   id INT UNSIGNED PRIMARY KEY
 );
-INSERT INTO tmp_6months_remove
   SELECT gc.contact_id
   FROM civicrm_group_contact gc
     LEFT JOIN tmp_6months m ON m.id = gc.contact_id
-  WHERE m.id IS NULL AND gc.group_id = @iced_group AND gc.status = 'Added';
+  WHERE m.id IS NULL AND gc.group_id = @iced_group AND gc.status = 'Added'
+  INTO OUTFILE '/var/lib/mysql-files/nightly-iced-remove-%NOW%.tsv';
+
+LOAD DATA INFILE  '/var/lib/mysql-files/nightly-iced-remove-%NOW%.tsv'
+IGNORE INTO TABLE tmp_6months_remove (id);
+
 SET @iced_removed = (SELECT sum(result)
 FROM
   (SELECT dontusSetGroupContact(@iced_group, id, 'Removed') result
--- sql/nightly/update-oneoff-groups.sql
+++ sql/nightly/update-oneoff-groups.sql
@@ -11,28 +11,34 @@ DELETE FROM civicrm_group_contact WHERE group_id = @group_id;
 
 DELETE FROM civicrm_subscription_history WHERE group_id = @group_id;
 
-INSERT INTO civicrm_group_contact (group_id, contact_id, status)
-  SELECT @group_id, t.contact_id, 'Added'
-  FROM (
-         SELECT ct.contact_id, max(ct.total_amount) max_total_amount
-         FROM civicrm_contribution ct
-           JOIN civicrm_group_contact gc ON gc.group_id = @member_group AND gc.status = 'Added' AND gc.contact_id = ct.contact_id
-           LEFT JOIN civicrm_contribution_recur cr ON cr.contact_id = ct.contact_id
-         WHERE ct.contribution_status_id = 1
-           AND ct.receive_date >= DATE_ADD(CURRENT_DATE(), INTERVAL -18 MONTH)
-           AND ct.is_test = 0
-           AND ct.contribution_recur_id IS NULL
-           AND cr.id IS NULL
-         GROUP BY ct.contact_id
-         HAVING count(ct.id) = @cnt
-       ) t
-  WHERE t.max_total_amount <= 500;
+SELECT @group_id, t.contact_id, 'Added'
+FROM (
+        SELECT ct.contact_id, max(ct.total_amount) max_total_amount
+        FROM civicrm_contribution ct
+          JOIN civicrm_group_contact gc ON gc.group_id = @member_group AND gc.status = 'Added' AND gc.contact_id = ct.contact_id
+          LEFT JOIN civicrm_contribution_recur cr ON cr.contact_id = ct.contact_id
+        WHERE ct.contribution_status_id = 1
+          AND ct.receive_date >= DATE_ADD(CURRENT_DATE(), INTERVAL -18 MONTH)
+          AND ct.is_test = 0
+          AND ct.contribution_recur_id IS NULL
+          AND cr.id IS NULL
+        GROUP BY ct.contact_id
+        HAVING count(ct.id) = @cnt
+      ) t
+WHERE t.max_total_amount <= 500 INTO OUTFILE '/var/lib/mysql-files/nightly-donor-groups-%NOW%.tsv';
 
-INSERT INTO civicrm_subscription_history (contact_id, group_id, method, status, date)
-  SELECT
-    contact_id, group_id, 'Admin', status, NOW()
-  FROM civicrm_group_contact
-  WHERE group_id = @group_id AND status = 'Added';
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-donor-groups-%NOW%.tsv' IGNORE
+INTO TABLE civicrm_group_contact (group_id, contact_id, `status`);
+
+
+SELECT
+  contact_id, group_id, 'Admin', status, NOW()
+FROM civicrm_group_contact
+WHERE group_id = @group_id AND status = 'Added' 
+INTO OUTFILE  '/var/lib/mysql-files/nightly-donor-groups-subscription-history-%NOW%.tsv';
+
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-donor-groups-subscription-history-%NOW%.tsv' IGNORE
+INTO TABLE civicrm_subscription_history (contact_id, group_id, method, `status`, `date`);
 
 
 SET @cnt = 2;
@@ -43,30 +49,34 @@ DELETE FROM civicrm_group_contact WHERE group_id = @group_id;
 
 DELETE FROM civicrm_subscription_history WHERE group_id = @group_id;
 
-INSERT INTO civicrm_group_contact (group_id, contact_id, status)
-  SELECT @group_id, t.contact_id, 'Added'
-  FROM (
-         SELECT ct.contact_id, max(ct.total_amount) max_total_amount
-         FROM civicrm_contribution ct
-           JOIN civicrm_group_contact gc ON gc.group_id = @member_group AND gc.status = 'Added' AND gc.contact_id = ct.contact_id
-           LEFT JOIN civicrm_contribution_recur cr ON cr.contact_id = ct.contact_id
-         WHERE ct.contribution_status_id = 1
-           AND ct.receive_date >= DATE_ADD(CURRENT_DATE(), INTERVAL -18 MONTH)
-           AND ct.is_test = 0
-           AND ct.contribution_recur_id IS NULL
-           AND cr.id IS NULL
-         GROUP BY ct.contact_id
-         HAVING count(ct.id) = @cnt
-       ) t
-  WHERE t.max_total_amount <= 500;
+SELECT @group_id, t.contact_id, 'Added'
+FROM (
+        SELECT ct.contact_id, max(ct.total_amount) max_total_amount
+        FROM civicrm_contribution ct
+          JOIN civicrm_group_contact gc ON gc.group_id = @member_group AND gc.status = 'Added' AND gc.contact_id = ct.contact_id
+          LEFT JOIN civicrm_contribution_recur cr ON cr.contact_id = ct.contact_id
+        WHERE ct.contribution_status_id = 1
+          AND ct.receive_date >= DATE_ADD(CURRENT_DATE(), INTERVAL -18 MONTH)
+          AND ct.is_test = 0
+          AND ct.contribution_recur_id IS NULL
+          AND cr.id IS NULL
+        GROUP BY ct.contact_id
+        HAVING count(ct.id) = @cnt
+      ) t
+WHERE t.max_total_amount <= 500
+INTO OUTFILE '/var/lib/mysql-files/nightly-donor-groups-group-two-%NOW%.tsv';
 
-INSERT INTO civicrm_subscription_history (contact_id, group_id, method, status, date)
-  SELECT
-    contact_id, group_id, 'Admin', status, NOW()
-  FROM civicrm_group_contact
-  WHERE group_id = @group_id AND status = 'Added';
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-donor-groups-group-two-%NOW%.tsv' IGNORE
+INTO TABLE civicrm_group_contact (group_id, contact_id, `status`);
 
+SELECT
+  contact_id, group_id, 'Admin', status, NOW()
+FROM civicrm_group_contact
+WHERE group_id = @group_id AND status = 'Added'
+INTO OUTFILE '/var/lib/mysql-files/nightly-donor-groups-two-history-%NOW%.tsv';
 
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-donor-groups-two-history-%NOW%.tsv' IGNORE
+INTO TABLE civicrm_subscription_history (contact_id, group_id, method, `status`, `date`);
 
 SET @cnt = 3;
 SET @member_group = 42;
@@ -76,7 +86,6 @@ DELETE FROM civicrm_group_contact WHERE group_id = @group_id;
 
 DELETE FROM civicrm_subscription_history WHERE group_id = @group_id;
 
-INSERT INTO civicrm_group_contact (group_id, contact_id, status)
 SELECT @group_id, t.contact_id, 'Added'
 FROM (
   SELECT ct.contact_id, max(ct.total_amount) max_total_amount
@@ -91,10 +100,17 @@ FROM (
   GROUP BY ct.contact_id
   HAVING count(ct.id) = @cnt
 ) t
-WHERE t.max_total_amount <= 500;
+WHERE t.max_total_amount <= 500
+INTO OUTFILE '/var/lib/mysql-files/nightly-donor-groups-three-%NOW%.tsv';
 
-INSERT INTO civicrm_subscription_history (contact_id, group_id, method, status, date)
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-donor-groups-three-%NOW%.tsv' IGNORE
+INTO TABLE civicrm_group_contact (group_id, contact_id, `status`);
+
 SELECT
   contact_id, group_id, 'Admin', status, NOW()
 FROM civicrm_group_contact
-WHERE group_id = @group_id AND status = 'Added';
+WHERE group_id = @group_id AND status = 'Added'
+INTO OUTFILE '/var/lib/mysql-files/nightly-donor-groups-three-history-%NOW%.tsv';
+
+LOAD DATA INFILE '/var/lib/mysql-files/nightly-donor-groups-three-history-%NOW%.tsv' IGNORE
+INTO TABLE civicrm_subscription_history (contact_id, group_id, method, `status`, `date`);
