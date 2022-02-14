f9f3ed618fbdfaac9900b49ba92b22bf3ddcd591
Michal Mach <michal@wemove.eu>
2022-02-14 09:43:08 +0100

    change memory engine in place to avoid full temp table error

--- analytics/110_petitions.sql
+++ analytics/110_petitions.sql
@@ -1,4 +1,5 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+SET SESSION internal_tmp_mem_storage_engine = 'Memory';
 
 INSERT INTO tmp_petition_metrics (campaign_id, activity, status, npeople)
   SELECT
