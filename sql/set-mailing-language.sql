1191f3c2e2ee43de00962b85ae9a654f2931ccde
Romain Thouvenin <romain@wemove.eu>
2023-05-24 13:41:43 +0200

    Also set the optout message when settings the language of mailings

--- sql/set-mailing-language.sql
+++ sql/set-mailing-language.sql
@@ -1,2 +1,8 @@
-update civicrm_mailing m join civicrm_value_speakout_integration_2 c on entity_id=m.campaign_id set  language= language_4 where language is NULL;
+UPDATE civicrm_mailing m
+  JOIN civicrm_value_speakout_integration_2 c ON entity_id=m.campaign_id
+  LEFT JOIN civicrm_mailing_component optout ON optout.name = CONCAT('OptOut-', SUBSTRING(language_4, 1, 2))
+  SET language = language_4,
+      optout_id = COALESCE(optout.id, 7)
+  WHERE language is NULL
+;
 update civicrm_mailing  set language="en_UK" where language="en_GB" and (name like "%UK-EN" or  name like "%UK-EN)");
